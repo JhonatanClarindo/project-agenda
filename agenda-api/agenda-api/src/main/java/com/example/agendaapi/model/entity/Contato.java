89fa90ce7a583cf0b39ec53368d6db35b1bf7455
JhonatanClarindo <jhonatancs97@hotmail.com>
2023-06-26 23:19:23 -0300

    ajustes

--- agenda-api/agenda-api/src/main/java/com/example/agendaapi/model/entity/Contato.java
+++ agenda-api/agenda-api/src/main/java/com/example/agendaapi/model/entity/Contato.java
@@ -1,16 +1,15 @@
 package com.example.agendaapi.model.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//import javax.persistence.*;
 
 @Entity
 @Getter
